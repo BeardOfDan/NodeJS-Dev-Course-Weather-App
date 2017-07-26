844268d12cfd7234222d97ade02df68b6571df69
Dan <DanielWRoberts@outlook.com>
2017-07-26 14:56:41 -0700

    Included a weather api
    Finished video section 4: 34 "Wiring Up Weather Search"

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const request = require('request');
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode');
@@ -22,5 +23,18 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     console.log(errorMessage);
   } else {
     console.log(JSON.stringify(results, undefined, 2));
+  }
+});
+
+// ddba06b83a81213457264e4d7c895941
+
+request({
+  url: "https://api.darksky.net/forecast/ddba06b83a81213457264e4d7c895941/37.939151,-121.9470726",
+  json: true
+}, (error, response, body) => {
+  if ((!error) && (response.statusCode === 200)) {
+    console.log(`Temperature: ${body.currently.temperature}`);
+  } else {
+    console.log("ERROR! Unable to fetch weather.");
   }
 });
